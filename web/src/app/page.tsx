6af095052059ef6270332ed3f6b88ee31a1e568e
JungSeunghun <31593322+JungSeunghun@users.noreply.github.com>
2024-06-13 18:16:01 +0900

    테스트 문구 추가

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -17,9 +17,12 @@ const Home: React.FC = () => {
       <div className={styles.logo}>
         <Image src={"/main/textlogo.svg"} width={180} height={52} alt='페이지데이 로고'/>
       </div>
-      <button onClick={handleKakaoLogin}>
+      <button className={styles.kakaoButton} onClick={handleKakaoLogin}>
         <Image src={"/main/kakaoLogin.png"} width={300} height={42} alt='카카오 로그인 버튼'/>
       </button>
+      <div>
+        <p className={styles.description}>현재 테스트 중 입니다.</p>
+      </div>
     </div>
   );
 };
